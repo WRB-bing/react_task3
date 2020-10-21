6d1a6b4885bcefc41bf8ff0f92e44cfa59677dd8
wangruobing <1400121920@qq.com>
2020-10-21 18:23:30 +0800

    wangruobing commit for task3

---
+++ build/precache-manifest.76c9b1510cf59796d427f7f1581bad9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99379680bca4da895905e12378cdb5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64c73d6b5704b438cd23",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "9169bda6aa5496cbded2",
+    "url": "/static/css/main.5f10e690.chunk.css"
+  },
+  {
+    "revision": "64c73d6b5704b438cd23",
+    "url": "/static/js/2.369b1990.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.369b1990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9169bda6aa5496cbded2",
+    "url": "/static/js/main.387c0dd5.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "f2f8a9673fb38995d413816954524bf9",
+    "url": "/static/media/item.f2f8a967.jpg"
+  }
+]);
